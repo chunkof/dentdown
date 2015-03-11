72bcfe84ad0fbe4e5b365c23505fb134e3b21181
chunk offset <chunkoffset@gmail.com>
2015-03-11 23:58:03 +0900

    [clean]Local Utility

--- indent_den.js
+++ indent_den.js
@@ -4,6 +4,10 @@ function IndentDen(spec){
 
 (function() {
 "use strict";
+
+//---------------
+// IndentDen
+//---------------
 IndentDen.prototype =
 {
 // Initialize
@@ -12,23 +16,33 @@ IndentDen.prototype =
     },
 // Public
     setSpec:function(spec){
-        
+        spec = ut.or(spec,[]);
+        this._indent = ut.or(spec.indent, "  ");
     },
     convert:function(src){
         return src+"!";
     },
 // Internal
     
-// Utility
-    _or:function(spec, default_spec){
+// end of object
+    __end__:true
+}
+
+//---------------
+// Local Utility
+//---------------
+var ut =
+{
+    or:function(spec, default_spec){
         if (typeof spec === "undefined") {
             return default_spec;
         }
         return spec;
     },
 // end of object
-    __end:true
+    __end__:true
 }
+
 
 
 }());
