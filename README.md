4d8bb9c22e476e10e1a50772a7f74a8863169eec
chunk offset <chunkoffset@gmail.com>
2015-03-14 00:44:12 +0900

    [add]Enable convert to Markdown/HTML/Preview.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Indedent
+dentdown
 ==========
 
 now making...
--- dentdown.js
+++ dentdown.js
@@ -1,79 +1,82 @@
-function DentDown(spec) {
-    this.initialize(spec);
+function Dentdown(spec) {
+  this.initialize(spec);
 };
 
 (function () {
 "use strict";
 //---------------
-// DentDown
+// Dentdown
 //---------------
-DentDown.prototype =
+Dentdown.prototype =
 {
 // Initialize
-    initialize:function(spec){
-        this._s = [];
-        this.setSpec(spec);
-    },
+  initialize:function(spec){
+    this._s = [];
+    this.setSpec(spec);
+  },
 // Public
-    setSpec:function(spec){
-        spec = ut.or(spec,[]);
-        this._s.indent      = ut.or(spec.indent,    "  ");
-        this._s.line_feed   = ut.or(spec.line_feed, "\n");
-    },
-    convert:function(src){
-        var dst = "";
-        var lines = src.split(this._s.line_feed);
-        for (var i = 0; i < lines.length; i++ ) {
-            var line = lines[i];
-            // get param
-            var line_info = this._getLineInfo(line);
-            // convert
-            
-            // output
-            dst += line_info.body + this._s.line_feed;
-        }
-        
-        return dst;
-    },
+  setSpec:function(spec){
+    spec = ut.or(spec,[]);
+    this._s.indent    = ut.or(spec.indent,  "  ");
+    this._s.line_feed   = ut.or(spec.line_feed, "\n");
+  },
+  toMarkdown:function(src){
+    var dst = "";
+    var lines = src.split(this._s.line_feed);
+    for (var i = 0; i < lines.length; i++ ) {
+      var line = lines[i];
+      // get param
+      var line_info = this._getLineInfo(line);
+      // output
+      dst += line_info.body + this._s.line_feed;
+    }
+    
+    return dst;
+  },
+  toHTML:function(src){
+    var md = this.toMarkdown(src);
+    
+    return markdown.toHTML(md);
+  },
 // Internal
-    _getLineInfo:function(line){
-        // parse indent
-        var indent_count = 0;
-        var stack = "";
-        var body  = "";
-        for (var i = 0; i < line.length; i++){
-            stack += line.charAt(i);
-            if (stack.length == this._s.indent.length)
-            {
-                if (stack == this._s.indent)
-                {
-                    ++indent_count;
-                    stack = "";
-                }
-                else
-                {
-                    body = line.substring(i-1);;
-                }
-            }
-        }
-        // parse type
-        var index = body.search(/^#/);
-        if (-1 != index)
+  _getLineInfo:function(line){
+    // parse indent
+    var indent_count = 0;
+    var stack = "";
+    var body  = "";
+    for (var i = 0; i < line.length; i++){
+      stack += line.charAt(i);
+      if (stack.length == this._s.indent.length)
+      {
+        if (stack == this._s.indent)
         {
-            var tag_s = "<h"  + (indent_count+1) + ">";
-            var tag_e = "</h" + (indent_count+1) + ">";
-            var value = body.substring(index+2);
-            body = tag_s + value + tag_e;
+          ++indent_count;
+          stack = "";
+          continue;
         }
         
-        // return result
-        return {
-            indent_count:indent_count,
-            body:body
-        };
-    },
+        break;
+      }
+    }
+    // trim body
+    body = line.substring(indent_count*this._s.indent.length);
+    
+    // parse type
+    var exist_h = body.search(/^#/);
+    if (-1 != exist_h)
+    {
+      var mark  = ut.repeatStr("#",indent_count);
+      body = mark + body;
+    }
+    
+    // return result
+    return {
+      indent_count:indent_count,
+      body:body
+    };
+  },
 // end of object
-    __end__:true
+  __end__:true
 };
 
 //---------------
@@ -81,14 +84,21 @@ DentDown.prototype =
 //---------------
 var ut =
 {
-    or:function(spec, default_spec){
-        if (typeof spec === "undefined") {
-            return default_spec;
-        }
-        return spec;
-    },
+  or:function(spec, default_spec){
+    if (typeof spec === "undefined") {
+      return default_spec;
+    }
+    return spec;
+  },
+  repeatStr:function(str, num){
+    var ret = "";
+    for (var i=0; i<num; ++i){
+    ret += str;
+    }
+    return ret;
+  },
 // end of object
-    __end__:true
+  __end__:true
 };
 
 
